cc6922c7a33f4613a5aaf8eea9dffe7feecdf3f5
Robin <contact@robin-d.fr>
2020-12-03 17:34:55 +0100

    minor change

--- webpack.config.js
+++ webpack.config.js
@@ -1,113 +1,114 @@
-require('webpack')
+require('webpack');
 
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 // const WebpackPwaManifest = require('webpack-pwa-manifest')
-const CssoWebpackPlugin = require('csso-webpack-plugin').default
+const CssoWebpackPlugin = require('csso-webpack-plugin').default;
 
 const config = {
   mode: 'development',
-  entry: [
-    './src/js/app.js'
-  ],
+  entry: ['./src/js/app.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
-    rules: [{
-      test: /\.s?[ac]ss$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader', // translates CSS into CommonJS modules
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              sourceMap: true,
+              plugins: function () {
+                return [
+                  require('postcss-flexbugs-fixes'),
+                  require('autoprefixer'),
+                ];
+              },
+            },
+          },
+          {
+            loader: 'sass-loader', // compiles SASS to CSS
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf|woff2|woff|eot)$/,
+        use: {
+          loader: 'file-loader',
           options: {
-            sourceMap: true
-          }
+            esModule: false,
+          },
         },
-        {
-          loader: 'postcss-loader', // Run post css actions
+      },
+      {
+        test: /.html$/,
+        use: {
+          loader: 'html-loader',
           options: {
-            sourceMap: true,
-            plugins: function () {
-              return [
-                require('postcss-flexbugs-fixes'),
-                require('autoprefixer')
-              ]
-            }
-          }
+            interpolate: true,
+            esModule: false,
+          },
         },
-        {
-          loader: 'sass-loader', // compiles SASS to CSS
-          options: {
-            sourceMap: true
-          }
-        }
-      ]
-    },
-    {
-      test: /\.(png|jpg|gif|svg|ttf|woff2|woff|eot)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          esModule: false
-        }
-      }
-    },
-    {
-      test: /.html$/,
-      use: {
-        loader: 'html-loader',
-        options: {
-          interpolate: true,
-          esModule: false
-        }
-      }
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader', // transpile to ES5
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }]
-    }
-    ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader', // transpile to ES5
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CssoWebpackPlugin({ pluginOutputPostfix: 'min' }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([{ from: './src/demo/html/img', to: 'img' }]),
     new MiniCssExtractPlugin({
-      filename: 'app.css'
+      filename: 'app.css',
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/demo/html/index.html'
+      template: './src/demo/html/index.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'invoice.html',
-      template: './src/demo/html/invoice.html'
+      template: './src/demo/html/invoice.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'elements.html',
-      template: './src/demo/html/elements.html'
+      template: './src/demo/html/elements.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'tunnel.html',
-      template: './src/demo/html/tunnel.html'
+      template: './src/demo/html/tunnel.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'page.html',
-      template: './src/demo/html/page.html'
-    })
+      template: './src/demo/html/page.html',
+    }),
 
     /**
         new WebpackPwaManifest({
@@ -123,8 +124,8 @@ const config = {
               }
             ]
         }) **/
-  ]
-}
+  ],
+};
 
 var devConfig = Object.assign({}, config, {
   mode: 'development',
@@ -132,17 +133,17 @@ var devConfig = Object.assign({}, config, {
     contentBase: path.join(__dirname, 'dist'),
     open: true,
     openPage: 'index.html',
-    port: 3000
-  }
-})
+    port: 3003,
+  },
+});
 
 var prodConfig = Object.assign({}, config, {
   mode: 'production',
   output: Object.assign({}, config.output, {
-    filename: 'app.min.js'
-  })
-})
+    filename: 'app.min.js',
+  }),
+});
 
 module.exports = [
-  devConfig//, prodConfig
-]
+  devConfig, //, prodConfig
+];
